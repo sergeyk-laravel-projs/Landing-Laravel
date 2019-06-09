d755335973f5f9dc91bc0b4b6175f758a640b5c1
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2019-06-09 21:41:13 +0000

    Laravel 05__Созданы модели. Выборка информации из таблиц базы данных

--- landing.laravel.loc/app/Http/Controllers/IndexController.php
+++ landing.laravel.loc/app/Http/Controllers/IndexController.php
@@ -6,13 +6,30 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
+
+
 class IndexController extends Controller
 {
     //
     
     public function execute(Request $request) {
+    	
+    	$pages = Page::all();
+    	$portfolios = Portfolio::get(array('name','filter','images'));
+    	$services = Service::where('id','<',20)->get();
+    	$peoples = People::take(3)->get();
+    	
+    	$menu = array();
+    	foreach($pages as $page) {
+			
+		}
+
 		
-		return view('layouts.site');
+		return view('site.index');
 	}
     
 }
