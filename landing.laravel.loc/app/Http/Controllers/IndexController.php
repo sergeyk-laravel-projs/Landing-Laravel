6839e4d1f3414ac41fbf67393732046c59629765
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2019-07-01 19:17:47 +0000

    Laravel 08__Механизм отправки почтовых сообщений

--- landing.laravel.loc/app/Http/Controllers/IndexController.php
+++ landing.laravel.loc/app/Http/Controllers/IndexController.php
@@ -21,7 +21,50 @@ class IndexController extends Controller
     //
     
     public function execute(Request $request) {
+    	
+    	
+    	
+    	if($request->isMethod('post')) {
 
+			$messages = [
+			
+			'required' => "Поле :attribute обязательно к заполнению",
+			'email' => "Поле :attribute должно соответствовать email адресу"
+			
+			];
+			
+			$this->validate($request,[
+			
+			'name' => 'required|max:255',
+			'email' => 'required|email',
+			'text' => 'required'	
+					
+			], $messages);
+			
+			
+			$data = $request->all();
+			
+			$result = Mail::send('site.email',['data'=>$data], function($message) use ($data) {
+				
+				$mail_admin = env('MAIL_ADMIN');
+				
+				$message->from($data['email'],$data['name']);
+				$message->to($mail_admin,'Mr. Admin')->subject('Question');
+				
+				
+			});
+			
+			if($result) {
+				return redirect()->route('home')->with('status', 'Email is send');
+			}
+			
+			//mail
+
+			
+			
+			
+		}
+    	
     	$pages = Page::all();
     	$portfolios = Portfolio::get(array('name','filter','images'));
     	$services = Service::where('id','<',20)->get();
